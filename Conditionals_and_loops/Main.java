8bebff0051f02fe55c587882af591409e50822af
Suyash Srivastava <srivastavsuyash123@gmail.com>
2025-08-30 09:20:35 +0530

    Update Main.java

--- Conditionals_and_loops/Main.java
+++ Conditionals_and_loops/Main.java
@@ -13,11 +13,22 @@ public class Main {
     */
 
     int salary = 25400;
-    if(salary>10000){
-      salary = salary + 2000;
+    // if(salary>10000){
+    //   salary = salary + 2000;
+    // }
+    // else{
+    //   salary = salary + 1000;
+    // }
+    // System.out.println(salary);
+
+    if(salary > 10000){
+      salary += 2000;
+    }
+    else if(salary > 20000){
+      salary += 3000;
     }
     else{
-      salary = salary + 1000;
+      salary += 1000;
     }
     System.out.println(salary);
   }
