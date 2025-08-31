a204cb6d4ef5a5069430ea71d92ede3528452166
Suyash Srivastava <srivastavsuyash123@gmail.com>
2025-08-31 11:12:23 +0530

    Create reverse.java

---
+++ Conditionals_and_loops/reverse.java
@@ -0,0 +1,18 @@
+package Conditionals_and_loops;
+
+import java.util.Scanner;
+
+public class reverse {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int rev = 0;
+    while(n>0){
+      int rem = n%10;
+      rev = rev*10 + rem;
+      n = n/10;
+    }
+    System.out.println(rev);
+    in.close();
+  }
+}
