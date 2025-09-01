35e9684ac903ee690ac2124657f91ddd5d442106
Suyash Srivastava <srivastavsuyash123@gmail.com>
2025-09-01 23:55:59 +0530

    Create basic.java

---
+++ Switch_Statement/basic.java
@@ -0,0 +1,13 @@
+package Switch_Statement;
+
+import java.util.Scanner;
+
+public class basic {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String fruit = in.next();
+    if(fruit.equals("mango")){
+      
+    }
+  }
+}
